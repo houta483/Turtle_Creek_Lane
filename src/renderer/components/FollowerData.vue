45530ff70a70ae5f61ec9d2ed9b93b8ffd4f3bd2
Tanner Houghton <houta483@uchicago.edu>
2020-05-16 19:58:04 -0500

    minor adjustments

--- src/renderer/components/FollowerData.vue
+++ src/renderer/components/FollowerData.vue
@@ -32,34 +32,3 @@ export default {
 <style>
 
 </style>
-
-
-
-
-
-
-
-
-
-
-
-  <div>
-    <p class='selectData'> Please select the file containing your followers' data </p>
-    <input style="padding-left: 20px" class='file' id="file" ref="file" type="file" v-on:change="handleFile('followers')"/>
-    <input type="file" ref="directory" webkitdirectory v-on:change="handleDirectory()"/>
-    <button style="padding-left: -20px" class='runPythonButton' @click="clearFile('followers')"> Clear File </button>
-
-    <br>
-    <button 
-      @click="myMethod"
-      style="margin-left: 10px; width: 394px;"
-    >
-      Analyze Follower Data 
-    </button>
-    <br v-if="this.rendering == true">
-    <br v-if="this.rendering == true">
-    <div class='parent' style="{width: 100%;}">
-      <div class="loader" v-if="this.rendering == true"></div>
-    </div>
-    <p v-if="this.rendering == false && this.fileProcessed == true">File Processed: {{this.nameOfFile}}</p>
-  </div>
